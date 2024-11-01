a42bdbce0368a10b16c7ab01809ded6a3c6063c2
maferbonifacio <maria.goncalves261@etec.sp.gov.br>
2024-11-01 09:43:34 -0300

    Aula 01-11

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 
 import Exemplo1 from './src/exemplos/ex_01';
+import Exemplo2 from './src/exemplos/ex_02';
+
 import Atividade1 from './src/atividades/atv_01';
+import Atividade2 from './src/atividades/atv_02';
 
 export default function App() {
   return (
     <View style={styles.container}>
-     <Atividade1 />
+     <Exemplo2 />
       <StatusBar style="auto" />
     </View>
   );
